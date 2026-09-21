5d9768ebbb49f03aca380e54610e13f4755aa6a7
Thomas Stassin <sempai.thomas@gmail.com>
2026-09-21 11:18:41 +0200

    add jump

--- Assets/#Project/Scripts/PlayerControl.cs
+++ Assets/#Project/Scripts/PlayerControl.cs
@@ -5,29 +5,43 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 using System;
 
-
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
 
     //public InputActions actions;
 
-    public InputActionAsset actions;
-    public float speed = 1f;
+    private const string ACTION_MAP = "CubeActionsMap";
+    private const string JUMP_ACTION = "Jump";
+    private const string X_AXIS = "XAxis";
+
+    [SerializeField] private InputActionAsset actions;
+    [SerializeField] private InputAction jump;
+    [SerializeField] private float speed = 1f;
     private InputAction xAxis;
+    [SerializeField] private float jumpForce = 350f;
 
     void Awake()
     {
-        xAxis = actions.FindActionMap("CubeActionsMap").FindAction("XAxis");
+        jump = actions.FindActionMap(ACTION_MAP).FindAction(JUMP_ACTION);
+        xAxis = actions.FindActionMap(ACTION_MAP).FindAction(X_AXIS);
+
+        jump.performed += ctx => { OnJump(ctx); };
+    }
+
+    private void OnJump(InputAction.CallbackContext ctx)
+    {
+        GetComponent<Rigidbody>().AddForce(jumpForce * Vector3.up);
     }
 
     void OnEnable()
     {
-        actions.FindActionMap("CubeActionsMap").Enable();
+        actions.FindActionMap(ACTION_MAP).Enable();
     }
 
     void OnDisable()
     {
-        actions.FindActionMap("CubeActionsMap").Disable();
+        actions.FindActionMap(ACTION_MAP).Disable();
     }
 
     void Update()
