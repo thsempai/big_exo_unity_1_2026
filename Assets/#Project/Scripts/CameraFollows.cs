5ba48cd18d81a25488c2d82f20b97c9574cdccd4
Thomas Stassin <sempai.thomas@gmail.com>
2026-09-21 10:32:11 +0200

    add CameraFollows

---
+++ Assets/#Project/Scripts/CameraFollows.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [Tooltip("Offset on z axe between camera and target")]
+    [SerializeField] private float zOffset = -3f;
+    [SerializeField] private float speed = 5.5f;
+
+    void Update()
+    {
+        float z = target.position.z + zOffset;
+        Vector3 position = transform.position;
+        position.z = z;
+
+        transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+    }
+}
